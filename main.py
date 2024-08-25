80b46b3c624e36f1739b32a8d3d98b932e55863e
Thiago S. Brasil <thiagosbrasil@ua.pt>
2024-08-25 17:06:38 +0100

    refact

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-# import requests
-# from bs4 import BeautifulSoup
-# from extract_serie_a import extract_serie_a
-# from matchlogs import extract_matchlogs
-
-# url = 'https://fbref.com/en/squads/d9fdd9d9/Botafogo-RJ-Stats'
-# response = requests.get(url)
-# if response.status_code != 200:
-#     print(f"Failed to retrieve the page. Status code: {response.status_code}")
-#     exit()
-# soup = BeautifulSoup(response.text, 'html.parser')
-
-# extract_matchlogs(soup)
-# extract_serie_a(soup)
-
-from scraper import extract_table_data
+from table_scraper import extract_table_data
 
 # Lista da serie A
 extract_table_data(
